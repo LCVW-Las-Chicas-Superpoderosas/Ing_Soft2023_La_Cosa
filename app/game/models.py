ed2e9874642965b5536981ed87f44383a70f67ed
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-09-25 23:48:35 -0300

    feat: add Game view, create /game endpoint and modify Game model accordingly

--- app/game/models.py
+++ app/game/models.py
@@ -7,8 +7,13 @@ from pony.orm import PrimaryKey, Required, Set, Optional
 class Game(Models.Entity):
     id = PrimaryKey(int, auto=True)
     name = Required(str, unique=True, index=True)
-    password = Optional(str, nullable=True)  # Define password as an optional (nullable) string field
+    password = Optional(
+        str, nullable=True
+    )  # Define password as an optional (nullable) string field
     created_at = Required(datetime, default=datetime.utcnow)
-    chats = Required("Chat")
-    cards = Set("Card")
-    players = Set("Player")
+    host = Required(int)  # Player_id of the host
+    chats = Required('Chat')
+    cards = Set('Card', nullable=True)
+    players = Set('Player')
+    min_players = Required(int)
+    max_players = Required(int)
--- app/game/view.py
+++ app/game/view.py
@@ -1,46 +1,74 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from model_base import ModelBase
-from pony.orm import db_session
+from pony.orm import db_session, select
 
 from model_base import ModelBase
 from game.models import Game
 from player.models import Player
-from card.models import Card
 from chat.models import Chat
-from card.models import CardType
 
 
 from pydantic import BaseModel
-
 
 router = APIRouter()
 MODELBASE = ModelBase()
 
 
-# datos que llegan en la solicitud POST
 class GameRequest(BaseModel):
+    # Modeling the request body
+    id_player: int
     name: str
     password: str = ''
+    min_players: int
+    max_players: int
 
 
 @router.post('/game')
 def create_game(game_data: GameRequest):
-    with db_session:
-        chat = MODELBASE.add_record(Chat)  # chat de la sala de juego, ignorar por ahora
-        player = MODELBASE.add_record(
-            Player, name='Lauta'
-        )  # jugador que crea la sala de juego
-        card = MODELBASE.add_record(
-            Card, name='Lauta_card', type=CardType.PANIC.value
-        )  # carta que se le asigna al jugador que crea la sala de juego
-        game = MODELBASE.add_record(
-            Game,
-            name=game_data.name,
-            password=game_data.password,
-            chats=chat,
-            cards=card,
-            players=player,
-        )
+    try:
+        with db_session:
+            if (
+                game_data.min_players > game_data.max_players
+                or game_data.min_players < 4
+                or game_data.max_players > 12
+            ):
+                raise HTTPException(
+                    status_code=400,
+                    detail='Incorrect range of players. Please check the minimum and maximum player fields.',
+                )
 
-    # devolver un mensaje de éxito con el nombre de la sala de juego
-    return {'detail', f'La sala de juego: {game.name} ha sido creada exitosamente.'}
+            # Check if the player is already part of a game
+            player_host = MODELBASE.get_record_by_value(Player, id=game_data.id_player)
+            existing_participant_game = select(
+                g for g in Game if player_host in g.players
+            )
+            if existing_participant_game:
+                raise HTTPException(
+                    status_code=400, detail='User is already part of a game.'
+                )
+
+            # Not adding cards to the game for now, it is set to nullable in the model
+            chat = MODELBASE.add_record(Chat)
+            game = MODELBASE.add_record(
+                Game,
+                name=game_data.name,
+                password=game_data.password,
+                chats=chat,
+                players=player_host,
+                min_players=game_data.min_players,
+                max_players=game_data.max_players,
+                host=game_data.id_player,
+            )
+
+            return {
+                'status_code': 200,
+                'detail': f'Game {game.name} created successfully.',
+                'data': {
+                    'game_id': game.id,
+                },
+            }
+
+    except HTTPException as http_exception:
+        raise http_exception  # Re-raise HTTPExceptions to return as response
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
