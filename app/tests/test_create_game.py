4693c028c188609eb15e2584010618356cb33262
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-09-29 18:08:15 -0300

    fix: now tests can be run any number of times, it deletes the data it creates. Now using teardown and setupclass recommended by Juan in PR review

--- app/tests/test_create_game.py
+++ app/tests/test_create_game.py
@@ -1,6 +1,8 @@
 import unittest
 
+from create_mysql_db import create_database
 from fastapi.testclient import TestClient
+from game.models import Game
 from main import app
 from model_base import ModelBase, initialize_database
 from player.models import Player
@@ -8,8 +10,29 @@ from pony.orm import db_session
 
 
 class TestCreateGameEndpoint(unittest.TestCase):
-    initialize_database()
+
     client = TestClient(app)
+
+    @classmethod
+    def setUpClass(cls):
+        # Create and initialize the database
+        create_database('ing_2023')
+        initialize_database()
+
+    def clean_DB(self):
+
+        with db_session:
+            model_base = ModelBase()
+            model_base.delete_record(Game.get(name='catacumbas_ok'))
+            model_base.delete_record(Player.get(name='juan'))
+            model_base.delete_record(Player.get(name='pedro'))
+            model_base.delete_record(Player.get(name='tomi'))
+            model_base.delete_record(Player.get(name='pepe'))
+
+    @classmethod
+    def tearDownClass(cls):
+        test_instance = cls()
+        test_instance.clean_DB()
 
     '''
     The following test cases cover possible scenarios
@@ -23,17 +46,17 @@ class TestCreateGameEndpoint(unittest.TestCase):
 
     def test_create_game(self):
 
-        model_base = ModelBase()
-
         with db_session:
+            model_base = ModelBase()
             model_base.add_record(Player, name='juan')
+            player_id = model_base.get_record_by_value(Player, name='juan').id
 
         game_data = {
-            'id_player': 1,
+            'id_player': player_id,
             'name': 'catacumbas_ok',
             'password': '123456',
             'min_players': 4,
-            'max_players': 8,
+            'max_players': 8
         }
         response = self.client.post('/game', json=game_data)
 
@@ -47,17 +70,18 @@ class TestCreateGameEndpoint(unittest.TestCase):
 
     def test_create_game_min_players_greater_than_max_players(self):
 
-        model_base = ModelBase()
-
         with db_session:
+
+            model_base = ModelBase()
             model_base.add_record(Player, name='pedro')
+            player_id = model_base.get_record_by_value(Player, name='pedro').id
 
         game_data = {
-            'id_player': 2,
+            'id_player': player_id,
             'name': 'catacumbas_min_gt_max',
             'password': '123456',
             'min_players': 8,
-            'max_players': 4,
+            'max_players': 4
         }
         response = self.client.post('/game', json=game_data)
         self.assertEqual(response.status_code, 400)
@@ -69,17 +93,17 @@ class TestCreateGameEndpoint(unittest.TestCase):
 
     def test_create_game_min_players_less_than_4(self):
 
-        model_base = ModelBase()
-
         with db_session:
+            model_base = ModelBase()
             model_base.add_record(Player, name='tomi')
+            player_id = model_base.get_record_by_value(Player, name='tomi').id
 
         game_data = {
-            'id_player': 3,
+            'id_player': player_id,
             'name': 'catacumbas_min_gt_4',
             'password': '123456',
             'min_players': 1,
-            'max_players': 12,
+            'max_players': 12
         }
         response = self.client.post('/game', json=game_data)
         self.assertEqual(response.status_code, 400)
@@ -91,17 +115,17 @@ class TestCreateGameEndpoint(unittest.TestCase):
 
     def test_create_game_max_players_greater_than_12(self):
 
-        model_base = ModelBase()
-
         with db_session:
+            model_base = ModelBase()
             model_base.add_record(Player, name='pepe')
+            player_id = model_base.get_record_by_value(Player, name='pepe').id
 
         game_data = {
-            'id_player': 4,
+            'id_player': player_id,
             'name': 'catacumbas_max_gt_12',
             'password': '123456',
             'min_players': 4,
-            'max_players': 13,
+            'max_players': 13
         }
         response = self.client.post('/game', json=game_data)
         self.assertEqual(response.status_code, 400)
@@ -113,12 +137,15 @@ class TestCreateGameEndpoint(unittest.TestCase):
 
     def test_create_game_player_already_part_of_a_game(self):
 
+        with db_session:
+            player_id = ModelBase().get_record_by_value(Player, name='juan').id
+
         game_data = {
-            'id_player': 1,
+            'id_player': player_id,
             'name': 'catacumbas_player_already_part_of_a_game',
             'password': '123456',
             'min_players': 4,
-            'max_players': 6,
+            'max_players': 6
         }
         response = self.client.post('/game', json=game_data)
         self.assertEqual(response.status_code, 400)
