b03826a95a02dc19a24a1610e5546c43b2ce0f1a
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-09-30 21:43:06 -0300

    feat: populate DB with 4 players, a fifth player can be added to check functionality for the moment

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import argparse
 
 import uvicorn
+from card.models import Card, CardType
 from card.view import router as CardsRouter
 from chat.models import Chat
 from fastapi import FastAPI
+from game.models import Game
 from game.view import router as GameRouter
 from model_base import ModelBase, initialize_database
 from player.models import Player
@@ -19,10 +21,19 @@ app.include_router(PlayerRouter)
 @db_session
 def populate_db_test():
     model_base = ModelBase()
-    card = model_base.add_record(Card, name='lanzallamas', type=CardType.PANIC.value)
+    card1 = model_base.add_record(
+        Card, name='lanzallamas', type=CardType.PANIC.value)
+    player1 = model_base.add_record(Player, name='Gonzalo')
+    player2 = model_base.add_record(Player, name='Juan')
+    player3 = model_base.add_record(Player, name='Pedro')
+    player4 = model_base.add_record(Player, name='Maria')
+    player5 = model_base.add_record(Player, name='Lautaro')
+
     chat = model_base.add_record(Chat)
-    player = model_base.add_record(Player, name='test_con_los_pibes', cards=card)
-    model_base.add_record(Game, name='el_pepe_game4', password='', chats=chat, cards=card, players=player)
+    # add set of players to game
+    game = model_base.add_record(
+        Game, name='Bahamas', players=[player1, player2, player3, player4],
+        chats=chat, cards=card1, min_players=4, max_players=5, host=1)
 
 
 if __name__ == '__main__':
