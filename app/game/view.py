e5498cdcbe916d942179027ba0709fca2bdc2ddd
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-10-10 20:23:05 -0300

    feat: create endpoint that returns a list of joinable games

--- app/game/view.py
+++ app/game/view.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Header, HTTPException
 from game.models import Game, GameStatus
 from model_base import ModelBase
 from player.models import Player
-from pony.orm import db_session
+from pony.orm import db_session, select
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -175,7 +175,7 @@ def _is_game_startable(game: Game):
     # Check game status and number of players
     # return true if game can be started
     return (game.status == GameStatus.WAITING.value and
-        len(game.players) >= game.min_players)
+            len(game.players) >= game.min_players)
 
 
 @router.get('/game/join')
@@ -279,3 +279,26 @@ def delete_game(game_data: GameDeleteRequest):
         'detail': 'Game deleted successfully.',
         'data': {}
     }
+
+
+@router.get('/game/list')
+def get_games_list():
+    with db_session:
+        games = select(g for g in Game)[:]
+        games_list = []
+
+        for game in games:
+            if (game.status == GameStatus.WAITING.value and
+                    len(game.players) < game.max_players):
+                games_list.append({
+                    'game_id': game.id,
+                    'player_quantity': game.players.count(),
+                    'max_players': game.max_players,
+                    'name': game.name
+                })
+
+        return {
+            'status_code': 200,
+            'detail': 'Joinable games list.',
+            'data': games_list
+        }
