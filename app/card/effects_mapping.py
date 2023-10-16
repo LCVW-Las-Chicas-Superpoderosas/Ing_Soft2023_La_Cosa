635bc6977bb9e37ad5a6bee400e02e7f238cbabb
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-10-16 13:01:30 -0300

    feat: modify view.py to support playing cards that take no id and apply an effect to the game itself. Also add the mappinng in effects_mapping.py to invert the order of turns when the right card is played

--- app/card/effects_mapping.py
+++ app/card/effects_mapping.py
@@ -17,6 +17,20 @@ def flame_torch(target_id):
         return False
 
 
+# "watch_your_back" is a card that sets Game.clockwise to False
+def watch_your_back(player_id):
+    try:
+        with db_session:
+            player = MODEL_BASE.get_first_record_by_value(Player, id=player_id)
+            game = player.game
+            game.clockwise = not game.clockwise
+            game.flush()
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
 EFFECTS_TO_PLAYERS = {
-    'lanzallamas': flame_torch
+    'lanzallamas': flame_torch,
+    'vigila tus espaldas': watch_your_back
 }
