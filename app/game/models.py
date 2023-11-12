0dd80b926897b600c72088297aef1f3aafa91eb4
Jety <juan.carrillo.og@gmail.com>
2023-11-12 20:19:01 -0300

    add more cards

--- app/game/models.py
+++ app/game/models.py
@@ -275,6 +275,8 @@ class Game(Models.Entity):
             for player in self.players:
                 player.cards.add(ModelBase().get_first_record_by_value(Card, card_token='img81.jpg'))
                 player.cards.add(ModelBase().get_first_record_by_value(Card, card_token='img22.jpg'))
+                player.cards.add(ModelBase().get_first_record_by_value(Card, card_token='img81.jpg'))
+                player.cards.add(ModelBase().get_first_record_by_value(Card, card_token='img22.jpg'))
                 for _ in range(0, 4):
                     if initial_deck_shuffle[0].type == 3:
                         self.the_thing = player.id
